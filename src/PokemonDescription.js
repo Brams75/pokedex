616d55dfcba835298fc5ade9da410a1165c6ca1e
Ibrahim <abram.pomposelli1@gmail.com>
2021-04-04 22:24:43 +0200

    Add descriptions

--- src/PokemonDescription.js
+++ src/PokemonDescription.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import './PokemonDescription.scss';
+import './PokemonTypes';
 import axios from 'axios';
+import PokemonTypes from './PokemonTypes';
 
 const baseUrl = 'https://pokeapi.co/api/v2/pokemon/';
 
@@ -15,10 +17,12 @@ const PokemonDescription = ({ name }) => {
       console.error(error);
     }
   };
+
   useEffect(() => {
     getPokemon();
     // eslint-disable-next-line
   }, []);
+
   if (Object.entries(pokemon).length === 0) {
     return <p>Hello</p>;
   }
@@ -32,6 +36,19 @@ const PokemonDescription = ({ name }) => {
       <div className="PokemonDescription__image">
         <img src={pokemon.sprites.front_default} alt={pokemon.name} />
       </div>
+      <p>height : {pokemon.height} inch</p>
+      <p>weight : {pokemon.weight} once</p>
+      <div>
+        Stats
+        <ul>
+          {pokemon.stats.map((stat) => (
+            <li key={stat.stat.name}>
+              {stat.stat.name} : {stat.base_stat}
+            </li>
+          ))}
+        </ul>
+      </div>
+      <PokemonTypes pokemonTypes={pokemon.types} />
     </div>
   );
 };
